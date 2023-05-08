f41d9c2c4280641a965b66d0fe31f2a4c4b4d832
Anil Gulati <anilfromdit@gmail.com>
2023-05-08 09:10:27 +0530

    client side module started

--- client side/excesslab/src/App.js
+++ client side/excesslab/src/App.js
@@ -1,30 +1,10 @@
 
-import './App.css';
-import { Routes, Route, BrowserRouter } from "react-router-dom"
-import Home from "./components/Home/Home.jsx"
-import Header from "./components/Layouts/Header.jsx";
-import Footer from "./components/Layouts/Footer.jsx";
-import Login from "./components/Auth/Login.jsx";
-import SignUp from "./components/Auth/SignUp.jsx";
-import Apps from "./components/Apps/Apps.jsx";
-import Dashboard from "./components/Dashboard/Dashboard.jsx";
-import WIP from "./components/Extras/WIP.jsx";
+import data from './data.json'
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/apps" element={<Apps />} />
-          <Route path="/*" element={<WIP />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
+    <>
+      {data.map((row) => { return <h1>{row.login}</h1> })}
+    </>
   );
 }
 
--- client side/excesslab/src/components/Home/Home.jsx
+++ client side/excesslab/src/components/Home/Home.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 import './home.css'
 const Home = () => {
   return (
-    <div class="hero">
-    <div class="hero-text-box">
-      <h1 class="heading-primary">
+    <div className="hero">
+    <div className="hero-text-box">
+      <h1 className="heading-primary">
       Simplify your <span>backend</span> infrastructure with <span> Excess</span> Labs
       </h1>
-      <p class="hero-description">
+      <p className="hero-description">
       Our comprehensive BaaS platform offers everything you need to build and deploy your applications quickly and efficiently.
       </p>
-      <a href="/apps" class="btn btn--fill margin-right-btn"
+      <a href="/apps" className="btn btn--fill margin-right-btn"
         >Create App
       </a>
-      <a href="#" class="btn btn--outline margin-right-btn"
+      <a href="#" className="btn btn--outline margin-right-btn"
         >Learn more &darr;
       </a>
     </div>
-    <div class="hero-img-box">
+    <div className="hero-img-box">
       
     </div>
-    <div class="delivered-meals">
-      <div class="delivered-imgs">
+    <div className="delivered-meals">
+      <div className="delivered-imgs">
         <img src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-1.jpg" alt="Customer photo" />
         <img src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-2.jpg" alt="Customer photo" />
         <img src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-3.jpg" alt="Customer photo" />
@@ -29,7 +29,7 @@ const Home = () => {
         <img src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-5.jpg" alt="Customer photo" />
         <img src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/customers/customer-6.jpg" alt="Customer photo" />
       </div>
-      <p class="delivered-text">
+      <p className="delivered-text">
         <span>99+</span> Apps Created This Week!
       </p>
     </div>
